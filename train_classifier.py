5b2a628a6b436bf0e99841da7325230c2487db92
Robert Walters <waltersmac@gmail.com>
2020-05-10 22:36:22 +0100

    Adding intial file for train classifier script

---
+++ train_classifier.py
@@ -0,0 +1,59 @@
+# import libraries
+import nltk
+nltk.download(['punkt', 'wordnet'])
+
+import re
+import numpy as np
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sqlalchemy import create_engine
+
+
+
+# load data from database
+engine = create_engine('sqlite:///InsertDatabaseName.db')
+df = pd.read_sql_table("MessagesCatTable", con=engine)
+X = df.message.values
+y = df[df.columns[4:]].values
+
+
+labels = df.columns[4:]
+
+
+
+# tokenization function to process your text data
+def tokenize(text):
+    
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+
+# build pipeline
+pipeline = Pipeline([
+    ('vect', CountVectorizer(tokenizer=tokenize)),
+    ('tfidf', TfidfTransformer()),
+    ('clf', MultiOutputClassifier(KNeighborsClassifier()))
+])
+
+
+
+
+# Train pipeline - Split data into train and test sets, the train the pipeline
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+pipeline.fit(X_train, y_train)
