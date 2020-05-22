fd42c493d2c5f8dc38504c3f75ebb5cc468e14a3
Robert Walters <waltersmac@gmail.com>
2020-05-22 22:51:57 +0100

    Updating train classifier script

--- train_classifier.py
+++ train_classifier.py
@@ -51,8 +51,6 @@ pipeline = Pipeline([
 ])
 
 
-
-
 # Train pipeline - Split data into train and test sets, the train the pipeline
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
